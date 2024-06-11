e65acef7c78c2c3544648c1b66ab3c10fb4b3164
mubarak olalekan <mubarakolalekan316@gmail.com>
2024-06-11 06:32:21 +0100

    ffew minutes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const textBox = document.getElementById("textBox");
 const toFahrenheit = document.getElementById("toFahrenheit");
 const toCelsius = document.getElementById("toCelsius");
 const result = document.getElementById("result");  
+const result2 = document.getElementById("result2");  
 let temp;
 
 function convert(){
@@ -10,13 +11,26 @@ if(toFahrenheit.checked){
     temp = Number(textBox.value)
     temp = temp * 9/5 + 32
 
-    result.textContent = temp + ""
+    result.textContent = temp + `\u00B0F`
 
    
-} if else(toCelsius.checked){
-    result.textContent = "enter a unit"
+} else if(toCelsius.checked){
+    temp = Number(textBox.value)
+    temp = (temp - 32) * 5/9
+
+    result.textContent = temp + `\u00B0C`
     
-}else{
+}else if(toFahrenheit.checked && toCelsius.checked){
+    temp = temp * 9/5 + 32
+
+    result.textContent = temp + `\u00B0F`
+     
+    temp = (temp - 32) * 5/9
+
+    result2.textContent = temp + `\u00B0C`
+
+}
+else{
     result.textContent = "select a unit"
 }
 }
