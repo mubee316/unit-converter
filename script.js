fd3a7682422590f6d4ae4a28c8d398af275d9db0
mubarak olalekan <mubarakolalekan316@gmail.com>
2024-06-11 03:41:35 +0100

    still cooking

---
+++ script.js
@@ -0,0 +1,7 @@
+const textBox = document.getElementById("textBox")
+const toFahrenheit = document.getElementById("toFahrenheit")
+const toCelsius = document.getElementById("toCelsius")
+
+function convert(){
+    
+}
