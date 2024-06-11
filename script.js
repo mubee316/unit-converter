c304111c240a01be4179269c473e0f4a738d6c9b
mubarak olalekan <mubarakolalekan316@gmail.com>
2024-06-11 04:21:07 +0100

    almost there

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const textBox = document.getElementById("textBox")
 const toFahrenheit = document.getElementById("toFahrenheit")
 const toCelsius = document.getElementById("toCelsius")
+const result = document.getElementById("result")  
+let temp;
 
 function convert(){
-    
+if(toFahrenheit.checked){
+    result.textContent = "i am a man"
+}else{
+    result.textContent = "i ama aman"
+}
 }
